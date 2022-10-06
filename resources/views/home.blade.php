137a5b70948fb2437ca4c2df6e634e62d8cd733f
ULTR4M4N <p4nthenigma@gmail.com>
2022-10-06 11:31:53 +0800

    #6 Changed welcome text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div class="container">
 	<div class="col-md-12">
-		<h1 class="text-center" style="padding: 20px;">Welcome!</h1>
+		<h1 class="text-center" style="padding: 20px;">Welcome to Restest!</h1>
 	</div>
 </div>
 @endsection
